f9f05bb9eb2ca22e8d530d666b5f2b2c344fc9a5
hariharen96 <hariharen1996@gmail.com>
2024-12-16 19:53:04 +0530

    feat: update home page with js,styles

--- academic_hub/settings.py
+++ academic_hub/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATIC_URL = 'static/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
--- dashboard/static/dashboard/js/script.js
+++ dashboard/static/dashboard/js/script.js
@@ -1,12 +1,31 @@
+const profileBtn = document.querySelector(".create-profile");
+
 document.addEventListener("DOMContentLoaded", (e) => {
   setTimeout(() => {
     const message = document.querySelectorAll(".alert-message");
     message.forEach((items) => {
       items.style.opacity = 0;
-      items.style.transition = 'opacity 1s ease'
+      items.style.transition = "opacity 1s ease";
       setTimeout(() => {
         items.style.display = "none";
       }, 1000);
     });
   }, 3000);
+});
+
+profileBtn.addEventListener("click", () => {
+  let profileImg = document.querySelector(".profile-img");
+  profileImg.classList.add("highlight-profile");
+
+  const toggleNav = document.querySelector(".navbar-toggler");
+
+  if (window.innerWidth < 992) {
+    if (toggleNav.getAttribute("aria-expanded") === "false") {
+      toggleNav.click();
+    }
+  }
+  setTimeout(() => {
+    profileImg.classList.remove("highlight-profile");
+    toggleNav.getAttribute("aria-expanded") === "true";
+  }, 5000);
 });
