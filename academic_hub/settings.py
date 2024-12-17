3bd9c1cdab451ab32dd9bbcef5ed9943c7b8b241
hariharen96 <hariharen1996@gmail.com>
2024-12-17 11:29:06 +0530

    feat: create db for profile and update profile image in ui

--- academic_hub/settings.py
+++ academic_hub/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,6 +123,10 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
--- academic_hub/urls.py
+++ academic_hub/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include 
 from users import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,6 @@ urlpatterns = [
     path("logout/",auth_views.logout_user,name="logout"),
     path('',include('dashboard.urls'))
 ]
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
